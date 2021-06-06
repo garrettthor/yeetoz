caafd462332c9b2a02ddd69bd65f47652ba50179
Garrett Thor <garrettthor@gmail.com>
2021-06-05 19:40:55 -0700

    form validation

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
+const { burritoValidateSchema } = require('./schemas.js')
+const catchAsync = require('./utilities/catchAsync');
+const ExpressError = require('./utilities/ExpressError');
 const methodOverride = require('method-override');
-const Burrito = require('./models/burrito')
+const Burrito = require('./models/burrito');
 const PORT = 1984;
 
 // Connection to the local database through mongoose.
@@ -21,6 +25,8 @@ db.once('open', () => {
 
 const app = express();
 
+
+
 // Set up using ejs-mate
 app.engine('ejs', ejsMate);
 // Setting up express to use ejs
@@ -31,6 +37,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateBurrito = (req, res, next) => {
+    const { error } = burritoValidateSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    };
+};
 
 
 // ROUTES
@@ -38,42 +53,56 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/burritos', async (req, res) => {
+app.get('/burritos', catchAsync(async (req, res) => {
     const burritos = await Burrito.find({});
     res.render('burritos/index', { burritos });
-});
+}));
 
 app.get('/burritos/new', (req, res) => {
     res.render('burritos/new');
 });
 
-app.post('/burritos', async (req, res) => {
+app.post('/burritos', validateBurrito, catchAsync(async (req, res, next) => {
+    // Rudimentary logic to detect if the body contains burrito at all
+    // if(!req.body.burrito) throw new ExpressError('Invalid Burrito Data', 400);
+
     const burrito = new Burrito(req.body.burrito);
     await burrito.save();
     res.redirect(`burritos/${burrito._id}`);
-});
+}));
 
-app.get('/burritos/:id', async (req, res) => {
+app.get('/burritos/:id', catchAsync(async (req, res) => {
     const burrito = await Burrito.findById(req.params.id);
     res.render('burritos/show', { burrito });
-});
+}));
 
-app.get('/burritos/:id/edit', async (req, res) => {
+app.get('/burritos/:id/edit', catchAsync(async (req, res) => {
     const burrito = await Burrito.findById(req.params.id);
     res.render('burritos/edit', { burrito });
-});
+}));
 
-app.put('/burritos/:id', async (req, res) => {
+app.put('/burritos/:id', validateBurrito, catchAsync(async (req, res) => {
     const { id } = req.params;
     const burrito = await Burrito.findByIdAndUpdate(id, { ...req.body.burrito }, { useFindAndModify: false });
     res.redirect(`${burrito._id}`);
-});
+}));
 
-app.delete('/burritos/:id', async (req, res) => {
+app.delete('/burritos/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Burrito.findByIdAndDelete(id, { useFindAndModify: false });
     res.redirect('/burritos');
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
 });
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No!  Something Went Terribly Wrong!';
+    res.status(statusCode).render('error', { err });
+});
+
 
 
 
