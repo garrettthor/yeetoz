38f95cbf111febc189ca2999291baee58925a1f8
Garrett Thor <garrettthor@gmail.com>
2021-06-12 11:01:31 -0700

    added star rating, moving onto img upload

--- routes/burritos.js
+++ routes/burritos.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+// Load controller for burritos
 const burritos = require('../controllers/burritos')
+// Load utilities
 const catchAsync = require('../utilities/catchAsync');
 // Load middleware
 const { isLoggedIn, isAuthor, validateBurrito } = require('../middleware')
 
 // Routes
-router.get('/', catchAsync(burritos.index));
+
+router.route('/')
+    .get(catchAsync(burritos.index))
+    // .post(isLoggedIn, validateBurrito, catchAsync(burritos.createBurrito));
+    .post(upload.single('image'),(req,res)=>{
+        console.log(req.body, req.file);
+        res.send('it worked!')
+    })
 
 router.get('/new', isLoggedIn, burritos.renderNewForm);
 
-router.post('/', isLoggedIn, validateBurrito, catchAsync(burritos.createBurrito));
+router.route('/:id')
+    .get(catchAsync(burritos.showBurrito))
+    .put(isLoggedIn, isAuthor, validateBurrito, catchAsync(burritos.updateBurrito))
+    .delete(isLoggedIn, isAuthor, catchAsync(burritos.deleteBurrito));
 
-router.get('/:id', catchAsync(burritos.showBurrito));
+
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(burritos.renderEditForm));
-
-router.put('/:id', isLoggedIn, isAuthor, validateBurrito, catchAsync(burritos.updateBurrito));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(burritos.deleteBurrito));
 
 module.exports = router;
